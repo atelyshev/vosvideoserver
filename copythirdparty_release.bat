1aa68844bd6066c022de5d5dae08dab8f2cf9602
atelyshev <atelyshev@vosvideo.com>
2015-01-02 19:39:43 -0500

    Move opencv under THIRD_PARTY

--- copythirdparty_release.bat
+++ copythirdparty_release.bat
@@ -1,5 +1,5 @@
 echo on
 IF NOT EXIST %1Release mkdir %1Release
 IF NOT EXIST %1Release\PocoFoundation.dll copy %THIRDPARTY_ROOT%\poco-1.4.7p1\lib\PocoFoundation.dll %1Release
-IF NOT EXIST %1Release\opencv_core2410.dll copy %OPENCV_DIR%\bin\opencv_core2410.dll %1Release
-IF NOT EXIST %1Release\opencv_highgui2410.dll copy %OPENCV_DIR%\bin\opencv_highgui2410.dll %1Release
+IF NOT EXIST %1Release\opencv_core2410.dll copy %THIRDPARTY_ROOT%\opencv\x86\vc12\bin\opencv_core2410.dll %1Release
+IF NOT EXIST %1Release\opencv_highgui2410.dll copy %THIRDPARTY_ROOT%\opencv\x86\vc12\bin\opencv_highgui2410.dll %1Release
