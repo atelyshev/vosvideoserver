0faa4d6a6409374b55988265b51e987b03572554
atelyshev <atelyshev@vosvideo.com>
2017-09-26 22:50:20 -0400

    Cleanup

---
+++ rtbcserver/hg_version.h
@@ -0,0 +1,4 @@
+#define HG_REVISION               44
+#define HG_LOCAL_MODIFICATIONS    1
+#define HG_CHANGESET              "Vos Video RTBC media server"
+#define HG_CHANGESET_SHORT        "da4d1cd19bdc"
